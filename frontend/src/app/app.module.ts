25b7e566bc711bc06df3a3885e2f82a92a7f32eb
Mariam <mariansaad585@gmail.com>
2023-06-08 14:36:40 +0300

    Auth structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './auth/components/log-in/log-in.component';
+import { SignUpComponent } from './auth/components/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LogInComponent, SignUpComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
