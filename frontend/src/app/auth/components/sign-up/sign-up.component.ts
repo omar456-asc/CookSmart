e2449b2a89c77cef77433a1d9d43b1a875f1de79
sara hussam <sarahussam203@gmail.com>
2023-06-08 17:38:21 +0300

    login

--- frontend/src/app/auth/components/sign-up/sign-up.component.ts
+++ frontend/src/app/auth/components/sign-up/sign-up.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+signup = true;
+login=false
+  filter(event: MouseEvent,type:string){
+    const links = document.querySelectorAll('div button');
+links.forEach(link => link.classList.remove('active'));
+const targetElement = event.target as HTMLElement; // cast event.target to the HTMLElement type
+if (HTMLElement) { // use optional chaining to check if event.target is not null
+  targetElement.classList.add('active'); // add the 'active' class to the clicked link
+}
+if(type === 'signup'){
+  this.signup=true;
+  this.login=false;
+}
+if(type === 'login'){
+  this.signup=false;
+  this.login=true;
+}
 
+  }
 }
