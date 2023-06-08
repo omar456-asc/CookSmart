45f23fcc0e0f8a44dabae86deb42d3937ff61400
sara hussam <sarahussam203@gmail.com>
2023-06-08 17:09:22 +0300

    route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './auth/components/log-in/log-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:LogInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
