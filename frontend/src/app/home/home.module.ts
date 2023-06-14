2310232fb2856677a8a58a280ced8f72e164304d
Sondos11 <sondossaid40@gmail.com>
2023-06-14 22:23:03 +0300

    create why-us component

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { HeaderComponent } from './components/header/header.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeroComponent, HeaderComponent],
+  declarations: [HomeComponent, HeroComponent, HeaderComponent, WhyUsComponent],
   imports: [CommonModule],
   exports: [HomeComponent, HeroComponent],
 })
