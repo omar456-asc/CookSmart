c382e04469b9e6176f9b8ab0648bfbf29112d499
Sondos11 <sondossaid40@gmail.com>
2023-06-14 22:47:08 +0300

    create contact-us component

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -6,9 +6,10 @@ import { HeaderComponent } from './components/header/header.component';
 import { WhyUsComponent } from './components/why-us/why-us.component';
 import { TeamMemberComponent } from './components/team-member/team-member.component';
 import { LatestBlogComponent } from './components/latest-blog/latest-blog.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeroComponent, HeaderComponent, WhyUsComponent, TeamMemberComponent, LatestBlogComponent],
+  declarations: [HomeComponent, HeroComponent, HeaderComponent, WhyUsComponent, TeamMemberComponent, LatestBlogComponent, ContactUsComponent],
   imports: [CommonModule],
   exports: [HomeComponent, HeroComponent],
 })
