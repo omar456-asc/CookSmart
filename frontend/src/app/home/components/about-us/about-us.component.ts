bb8de03c495cbc46a4227fbd07a2e7b53dbe61c7
nehad osman <nehad.osman19@gmail.com>
2023-06-19 01:51:48 +0300

    remove header and footer components from home

--- frontend/src/app/home/components/about-us/about-us.component.ts
+++ frontend/src/app/home/components/about-us/about-us.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about-us',
+  selector: 'app-about-us1',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
