735831eaf5c40a590ab613621852cfa0addc846b
Mahmoud Mohamed <mahmoudmahmed1499@gmail.com>
2023-06-19 18:46:07 +0300

    footer and nav in app-component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  constructor(private router: Router) { }
+
+  isAuthRoute(): boolean {
+    return this.router.url === '/auth';
+  }
 }
