2e8a4ea59fa3671015eb12f10cc55ce64eb942f1
nehad osman <nehad.osman19@gmail.com>
2023-06-19 23:01:04 +0300

    add multer and cloudinary to profile for upload image

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,11 +1,47 @@
 let usersmodel = require("../Models/usersModel");
 const bcrybt = require("bcrypt");
-// const multer = require("multer");
+const multer = require("multer");
 var path = require("path");
-// const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const uploadsCloud = (file, folder) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(
+      file,
+      (result) => {
+        resolve({
+          url: result.url,
+          id: result.public_id,
+        });
+      },
+      {
+        resource_type: "auto",
+        folder: folder,
+      }
+    );
+  });
+};
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix =
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    cb(null, uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 var GetAllUsers = async (req, res) => {
   try {
@@ -45,6 +81,30 @@ var UpdateUserByID = async (req, res) => {
   }
 };
 
+var UpdateUserProfileData = async (req, res) => {
+  try {
+    console.log("-------UpdateUserProfileData--------");
+    var updatedUser = req.body;
+
+    await usersmodel.updateOne(
+      { _id: updatedUser.id },
+      {
+        fname: updatedUser.fname,
+        lname: updatedUser.lname,
+        email: updatedUser.email,
+        mobile: updatedUser.mobile,
+        address: updatedUser.address,
+        gender: updatedUser.gender,
+        age: updatedUser.age,
+      }
+    );
+    await res.send(updatedUser);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send("*******failed to update new user**********");
+  }
+};
+
 
 var DeleteUserByID = async (req, res) => {
   try {
@@ -58,9 +118,38 @@ var DeleteUserByID = async (req, res) => {
 };
 
 
+var UploadProfilePic = async (req, res) => {
+  try {
+    const uploader = async (path) => await uploadsCloud(path, "Images");
+    const { path } = req.file;
+    const newPath = await uploader(path);
+    const avatarUrl = newPath.url;
+    await usersmodel.updateOne(
+      { _id: req.body.userId },
+      {
+        avatar: avatarUrl,
+      }
+    );
+
+    res.status(200).send({
+      messege: "Hello from upload profile pic",
+      url: avatarUrl,
+      status: "ok",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send("Error in upload profile pic");
+  }
+};
+
+
+
 module.exports = {
   GetAllUsers,
   UpdateUserByID,
   GetUserByID,
+  UpdateUserProfileData,
+  UploadProfilePic,
+  upload,
   DeleteUserByID
 };
