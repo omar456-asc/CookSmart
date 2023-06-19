50835262e5c0c3bcb09dce2e449b1e90a292fdbe
Mahmoud Mohamed <mahmoudmahmed1499@gmail.com>
2023-06-19 17:16:33 +0300

    fix router module in shared module

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,6 +13,7 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
