6bf4e1cb35f058c9c5b698615f5552bded2a074f
Omar Walid <omo_1712@yahoo.com>
2023-06-20 12:19:15 +0300

    add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { HomeModule } from './home/home.module';
+import { PaymentComponent } from './payment/payment.component';
+import { OrderComponent } from './order/components/order/order.component';
 
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'auth', component: AuthComponent },
   { path: '', component: HomeComponent },
+  {
+    path: 'payment/:id',
+    component: PaymentComponent,
+  },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'order', component: OrderComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
