ec5c660ce1910d9aa0fcabfce9a7811092144dea
Mariam <mariansaad585@gmail.com>
2023-06-21 19:36:53 +0300

    product routes

--- backend/Routes/productsRoutes.js
+++ backend/Routes/productsRoutes.js
@@ -1,12 +1,16 @@
 const ProductController = require("../Controllers/ProductController");
-// const RatingController = require("../Controllers/RatingController");
 const express = require("express");
 let router = express.Router();
 
 //#region Product
+router.get("/latest6products", ProductController.getLatest6products);
 router.get("/", ProductController.GetAllProducts);
 router.get("/:id", ProductController.GetProductByID);
 router.get("/search/:key", ProductController.SearchMeal);
+router.post("/:id",RatingController.addNewRate);
+router.delete("/:id", ProductController.DeleteProductByID);
+router.post("/", ProductController.addNewProduct);
+router.put("/:id", ProductController.editProduct);
 //#endregion
 
 module.exports = router;
