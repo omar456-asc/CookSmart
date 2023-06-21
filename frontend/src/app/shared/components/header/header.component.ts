0195bd8c28fad9f31db87cc1d0bea83e0cf02dc0
sara hussam <sarahussam203@gmail.com>
2023-06-21 19:20:39 +0300

    login-logout

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/log-in/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  constructor(private route: ActivatedRoute) {}
+export class HeaderComponent  {
+  userID:any
+  constructor(private route: ActivatedRoute,
+    private auth:AuthService,
+    private router: Router) {
+      this.userID=this.auth.getUserID()
+      console.log(this.userID)
+    }
+
+
+
   ngAfterViewInit() {
     this.route.fragment.subscribe((fragment) => {
       if (fragment) {
@@ -17,5 +28,10 @@ export class HeaderComponent {
         }
       }
     });
+  }
+  logout() {
+    localStorage.removeItem('id');
+    localStorage.removeItem('Token');
+    this.router.navigateByUrl('/auth')
   }
 }
