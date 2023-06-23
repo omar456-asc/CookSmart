94efbe632118b80414ebaad20c7134746a6e3e87
Omar Walid <omo_1712@yahoo.com>
2023-06-23 23:13:46 +0300

    remove recommand for guest

--- frontend/src/app/home/components/recommendation/recommendation.component.ts
+++ frontend/src/app/home/components/recommendation/recommendation.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/services/log-in/auth.service';
 
 @Component({
   selector: 'app-recommendation',
   templateUrl: './recommendation.component.html',
-  styleUrls: ['./recommendation.component.css']
+  styleUrls: ['./recommendation.component.css'],
 })
 export class RecommendationComponent {
+  ID = this.authService.getUserID();
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {}
 }
